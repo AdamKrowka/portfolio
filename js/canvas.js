2988f2e59de980d3f582ab8f875a7de718b6390b
AdamKrowka <45877288+AdamKrowka@users.noreply.github.com>
2019-01-12 21:21:18 +0100

    Add files via upload

--- js/canvas.js
+++ js/canvas.js
@@ -1,251 +1,104 @@
-var canvas = document.querySelector('canvas');
-var body = document.getElementById('footer');
-
-// var ile = 20;
-
-
+var canvas = document.getElementById('mainCanvas');
+var body = document.getElementById('hobby');
+var nav =  document.getElementById('nawigacja');
+var navWidth = nav.clientWidth;
 var height = body.clientHeight;
-var width = body.clientWidth - 20;
-canvas.height = height;
-canvas.width = width;
-var c = canvas.getContext('2d');
-var ile = Math.floor(height / 4);
-var mouseDistance = 100;
-var CircleCount = 200;
-
-
-
-
-function ColorRand(min, max) {
-    return Math.floor(Math.random() * (max - min + 1) + min);
+var width = body.clientWidth;
+canvas.height = height*5;
+canvas.width = width*5;
+var lineThick=10;
+var eraserWidth;
+var rangeValue = document.getElementById("LineWidth");
+rangeValue .oninput = function(){
+    lineThick = document.getElementById("LineWidth").value;
+    eraserWidth = lineThick*2;
 }
+var bar  = document.getElementById('toolBar');
+
+console.log(bar.clientHeight);
+
+var c = canvas.getContext('2d');
+
+var input = document.getElementById('inputColorPicker');
+var LineColor = input.style.backgroundColor;
 var mouse = {
     x: undefined,
     y: undefined
 }
+var inputTest = 0;
+var EraserTest = 0;
 
+$('#mainCanvas').click(function(){
+    var LineColor = input.style.backgroundColor;
+    mouse.x = event.x - navWidth;
+        mouse.y = event.y;
+        // c.rect(mouse.x -(lineThick/2),mouse.y - (lineThick/2),lineThick,lineThick);
+        c.fillStyle = LineColor;
+        // c.fill();
+        if (EraserTest == 0){
+        c.arc(mouse.x,mouse.y,lineThick/2,0,2 * Math.PI);
+        c.fill();
+        }
+        else {};
 
-window.addEventListener('mousemove', function (event) {
-    mouse.x = event.x;
-    mouse.y = event.y;
 })
 
 
+rangeValue.addEventListener('mousedown', function () {
+    c.strokeStyle = fillStyle = "rgba(0, 0, 0, 0)";;
+    c.fillStyle = fillStyle = "rgba(0, 0, 0, 0)";;
+    console.log('nie pisze')
+    
+});
+rangeValue.addEventListener('mouseup',function(){
+    var LineColor = input.style.backgroundColor;
+    c.strokeStyle = LineColor;
+            c.fillStyle = LineColor;
+            console.log('pisze')
+})
 
-function Circle(n, r, x, y, xd, yd, color, mass) {
-    this.mass = mass;
-    this.n = n;
-    this.r = r;
-    this.rNormal = r;
-    this.x = x;
-    this.y = y;
-    this.velocity = {
-        x: xd,
-        y: yd
-    }
-    this.xd = xd;
-    this.yd = yd;
-    this.color = color;
-    this.newColor = color;
 
-    this.drew = function () {
+window.addEventListener('mousedown', function (event) {
+    var LineColor = input.style.backgroundColor;
+    if (inputTest == 0) {
+        if (EraserTest == 0){
+            c.beginPath();
+            c.strokeStyle = LineColor;
+            c.fillStyle = LineColor;
+            c.arc(mouse.x,mouse.y,lineThick/2,0,2 * Math.PI);
+            c.fill();
+        }
+        if (EraserTest == 1)
+            c.strokeStyle = "white";
 
         c.beginPath();
-        c.arc(this.x, this.y, this.r, 0, Math.PI * 2, false)
-        c.fillStyle = this.color;
-        c.fill();
-        c.strokeStyle = this.newColor;
-        c.lineWidth = 2;
-        c.stroke();
     }
+    window.addEventListener('mousemove', function (event) {
+        mouse.x = event.x - navWidth;
+        mouse.y = event.y;
+        if (inputTest == 0)
+            line();
+    });
+});
+
+window.addEventListener('mouseup', function (event) {
+    c.strokeStyle = "transparent";
+    c.beginPath();
+});
 
 
-    this.bounce = function () {
-        this.x = this.x + this.velocity.x;
-        this.y = this.y + this.velocity.y;
-
-        // for (var c = 0; c < circles.length; c++) {
-        //     if (circles[c] == this) continue;
-
-        //     else if (getDistance(this.x, this.y, circles[c].x, circles[c].y) <= this.r + circles[c].r) {
-        //         resolveCollision(this, circles[c])
-        //     }
-        // }
 
 
-        if (this.x + this.r >= canvas.width || this.x - this.r <= 0) {
-            this.velocity.x = -this.velocity.x;
-        }
+function line() {
+    c.lineTo(mouse.x, mouse.y);
+    if (EraserTest == 0)
+        c.lineWidth = lineThick;
+    if (EraserTest == 1)
+        c.lineWidth =eraserWidth;
 
 
-        if (this.y + this.r >= canvas.height || this.y - this.r <= 0) {
-            this.velocity.y = -this.velocity.y;
-
-        }
-
-
-        if (mouse.x - this.x < mouseDistance && mouse.x - this.x > -mouseDistance &&
-            mouse.y - this.y < mouseDistance && mouse.y - this.y > -mouseDistance) {
-            if (this.color == this.newColor) {
-                // c.fillStyle = "transparent";
-                this.color = "transparent";
-
-
-            }
-        } else {
-            this.color = this.newColor;
-
-        }
-
-
-        this.drew();
-    }
-}
-
-
-var circles = [];
-
-// WYŚWIETLENIE WSSZYSTKICH PIŁEK NA RAZ;
-
-for (var i = 0; i < ile; i++) {
-    addCircle(i)
-}
-// var pierwszy = 0;
-
-
-function addCircle(n) {
-    var n = n;
-    var red = ColorRand(0, 255);
-    var green = ColorRand(0, 255);
-    var blue = ColorRand(0, 255);
-    var color = 'rgb(' + red + ' , ' + green + ' , ' + blue + ' ) ';
-
-    this.r = (Math.random() * (height / 100)) + 17;
-    // this.r = 28;
-    var mass = r;
-
-    var x = undefined;
-    var y = undefined;
-    var xd = (Math.random() - 0.5) * 4;
-    var yd = (Math.random() - 0.5) * 4;
-
-    for (var c = 0; c < circles.length; c++) {
-        if (getDistance(this.x, this.y, circles[c].x, circles[c].y) <= this.r + circles[c].r || x == undefined) {
-            var x = Math.random() * (canvas.width - 2 * r) + r;
-            var y = Math.random() * (canvas.height - 2 * r) + r;
-        }
-    }
-    circles.push(new Circle(n, r, x, y, xd, yd, color, mass));
-}
-
-function removeCircle(n) {
-    for (var z = 0; z < circles.length; z++) {
-        if (circles[z].n == n) {
-            circles.splice(z, 1)
-        }
-    }
-
-}
-
-
-function getDistance(x1, y1, x2, y2) {
-    var x = x1 - x2;
-    var y = y1 - y2;
-    return Math.sqrt(Math.pow(x, 2) + Math.pow(y, 2));
+    c.stroke();
 }
 
 
 
-function move() {
-    c.strokeStyle = 'transparent';
-    requestAnimationFrame(move);
-    c.clearRect(0, 0, canvas.width, canvas.height);
-    for (var i = 0; i < circles.length; i++) {
-        circles[i].bounce();
-        // circles[i].drew();  
-        c.fill();
-        c.stroke();
-    }
-
-
-
-
-    c.font = "30px Arial";
-    c.fillText(circles.length, 10, 50);
-
-}
-
-/**
- * Rotates coordinate system for velocities
- *
- * Takes velocities and alters them as if the coordinate system they're on was rotated
- *
- * @param  Object | velocity | The velocity of an individual particle
- * @param  Float  | angle    | The angle of collision between two objects in radians
- * @return Object | The altered x and y velocities after the coordinate system has been rotated
- */
-
-function rotate(velocity, angle) {
-    const rotatedVelocities = {
-        x: velocity.x * Math.cos(angle) - velocity.y * Math.sin(angle),
-        y: velocity.x * Math.sin(angle) + velocity.y * Math.cos(angle)
-    };
-
-    return rotatedVelocities;
-}
-
-/**
- * Swaps out two colliding particles' x and y velocities after running through
- * an elastic collision reaction equation
- *
- * @param  Object | particle      | A particle object with x and y coordinates, plus velocity
- * @param  Object | otherParticle | A particle object with x and y coordinates, plus velocity
- * @return Null | Does not return a value
- */
-
-function resolveCollision(particle, otherParticle) {
-    const xVelocityDiff = particle.velocity.x - otherParticle.velocity.x;
-    const yVelocityDiff = particle.velocity.y - otherParticle.velocity.y;
-
-    const xDist = otherParticle.x - particle.x;
-    const yDist = otherParticle.y - particle.y;
-
-    // Prevent accidental overlap of particles
-    if (xVelocityDiff * xDist + yVelocityDiff * yDist >= 0) {
-
-        // Grab angle between the two colliding particles
-        const angle = -Math.atan2(otherParticle.y - particle.y, otherParticle.x - particle.x);
-
-        // Store mass in var for better readability in collision equation
-        const m1 = particle.mass;
-        const m2 = otherParticle.mass;
-
-        // Velocity before equation
-        const u1 = rotate(particle.velocity, angle);
-        const u2 = rotate(otherParticle.velocity, angle);
-
-        // Velocity after 1d collision equation
-        const v1 = {
-            x: u1.x * (m1 - m2) / (m1 + m2) + u2.x * 2 * m2 / (m1 + m2),
-            y: u1.y
-        };
-        const v2 = {
-            x: u2.x * (m1 - m2) / (m1 + m2) + u1.x * 2 * m2 / (m1 + m2),
-            y: u2.y
-        };
-
-        // Final velocity after rotating axis back to original location
-        const vFinal1 = rotate(v1, -angle);
-        const vFinal2 = rotate(v2, -angle);
-
-        // Swap particle velocities for realistic bounce effect
-        particle.velocity.x = vFinal1.x;
-        particle.velocity.y = vFinal1.y;
-
-        otherParticle.velocity.x = vFinal2.x;
-        otherParticle.velocity.y = vFinal2.y;
-    }
-}
-
-
-move();
---
+++ js/scriptc.js
@@ -0,0 +1,62 @@
+
+
+var is_touch_device = 'ontouchstart' in document.documentElement;
+if(is_touch_device){
+
+ $("#styleMobilki").attr({href : "css/stylemobile.css"});
+ $("#stylePC").attr({href : ""});
+}
+else{
+    $("#styleMobilki").attr({href : ""});
+ $("#stylePC").attr({href : "css/style.css"});
+}
+
+
+var clearBtn = document.getElementById("CLEAR");
+clearBtn.onclick = function(){
+c.clearRect(0 , 0 ,canvas.width , canvas.height);
+};
+     
+
+
+
+  var buttonColor = document.getElementById("inputColorPicker");
+var eraser = document.getElementById('eraser');
+
+eraser.onclick = function () {
+    erase();
+};
+
+
+buttonColor.onfocus = function () {
+    StopDrowing();
+};
+buttonColor.onblur = function () {
+    StartDrowing();
+};
+
+
+function erase() {
+    if (EraserTest == 0) {
+        EraserTest = 1;
+        eraser.style.boxShadow = "0px -20px 20px 10px #1459FF";
+        canvas.style.cursor = "url(cursor.png), pointer";
+    } else {
+        EraserTest = 0;
+        eraser.style.boxShadow = "0px 0px 45px 12px rgba(255,255,255,0)";
+        canvas.style.cursor = "pointer";
+    }
+    console.log(EraserTest);
+}
+
+function StopDrowing() {
+    inputTest = 1;
+
+
+}
+
+function StartDrowing() {
+    inputTest = 0;
+
+
+}
